bc5fb720a119668c08ad0dff44bd059d60199996
ingJcbDev <ing.jcb.dev@gmail.com>
2022-06-16 18:47:34 -0500

    Se configura el routerLink

--- 3_2_0/AdminLTEconAngular/adminlte/src/app/app-routing.module.ts
+++ 3_2_0/AdminLTEconAngular/adminlte/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
-const routes: Routes = [];
+//Aqui van las rutas de la app
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'productos', component: ProductosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
